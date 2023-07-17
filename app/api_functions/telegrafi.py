7767feabd042f893e7b074a87676f24194b880fe
Drilon Sylejmani <drilon.sylejmani2@gmail.com>
2023-07-17 11:40:50 +0200

    Added channel not available if not admin

--- app/api_functions/telegrafi.py
+++ app/api_functions/telegrafi.py
@@ -5,88 +5,98 @@ import datetime
 from PIL import Image
 import io
 
+
+
 async def scrape_telegrafi(p_message_list, channel, username, is_private):
-    url_paths = [
-        "/lajme/",
-        "/sport/",
-        "/fun/",
-        "/teknologji/",
-        "/auto/",
-        "/kuzhina/",
-        "/shendetesi/",
-        "/stili/",
-        "/femra/",
-        "/kultura/",
-        "/magazina/",
-        "/ekonomi/",
-        "/bote/",
-    ]
-    count = 0
-    query_params = {}
-    
-    for argument in p_message_list[2:]:
-        splitted_argument = argument.split(":")
-        argument_name = splitted_argument[0]
-        argument_value = splitted_argument[1]
-        query_params[argument_name] = argument_value
+    telegrafi_channel = discord.utils.get(channel.guild.channels, name="telegrafi")
+    if isinstance(channel, discord.DMChannel) or channel.name.lower() == "telegrafi" or channel.permissions_for(username).administrator:
+        url_paths = [
+            "/lajme/",
+            "/sport/",
+            "/fun/",
+            "/teknologji/",
+            "/auto/",
+            "/kuzhina/",
+            "/shendetesi/",
+            "/stili/",
+            "/femra/",
+            "/kultura/",
+            "/magazina/",
+            "/ekonomi/",
+            "/bote/",
+        ]
+        count = 0
+        query_params = {}
+        
+        for argument in p_message_list[2:]:
+            splitted_argument = argument.split(":")
+            argument_name = splitted_argument[0]
+            argument_value = splitted_argument[1]
+            query_params[argument_name] = argument_value
 
-    if is_private:
-        await username.send("Please wait. This scraper may take a while ...")
+        if is_private:
+            await username.send("Please wait. This scraper may take a while ...")
+        else:
+            await telegrafi_channel.send("Please wait. This scraper may take a while ...")
+
+        for url_path in url_paths:
+            query_params["url_path"] = url_path
+            response = requests.get(
+                url="http://localhost:8000/telegrafi/scrape",
+                params=query_params
+            )
+            count += len(response.json())
+
+        embed = Embed(title="Scraping Status", description=f"Done: Scraped {count} articles from Telegrafi website.", color=discord.Color.red())
+        if is_private:
+            await username.send(embed=embed)
+        else:
+            await telegrafi_channel.send(embed=embed)
     else:
-        await channel.send("Please wait. This scraper may take a while ...")
-
-    for url_path in url_paths:
-        query_params["url_path"] = url_path
-        response = requests.get(
-            url="http://localhost:8000/telegrafi/scrape",
-            params=query_params
-        )
-        count += len(response.json())
-
-    embed = Embed(title="Scraping Status", description=f"Done: Scraped {count} articles from Telegrafi website.", color=discord.Color.red())
-    if is_private:
-        await username.send(embed=embed)
-    else:
-        await channel.send(embed=embed)
+        await channel.send("This function can be used only in telegrafi channel or in a private chat")
 
 
 async def data_telegrafi(p_message_list, channel, username, is_private):
-    query_params = {}
-    for argument in p_message_list[2:]:
-        splitted_argument = argument.split(":")
-        argument_name = splitted_argument[0]
-        argument_value = splitted_argument[1]
-        query_params[argument_name] = argument_value
+    telegrafi_channel = discord.utils.get(channel.guild.channels, name="telegrafi")
+    if isinstance(channel, discord.DMChannel) or channel.name.lower() == "telegrafi" or channel.permissions_for(username).administrator:
+        query_params = {}
+        for argument in p_message_list[2:]:
+            splitted_argument = argument.split(":")
+            argument_name = splitted_argument[0]
+            argument_value = splitted_argument[1]
+            query_params[argument_name] = argument_value
 
-    response = requests.get(
-        url="http://localhost:8000/telegrafi/data",
-        params=query_params
-    )
-    
-    results = response.json()["results"]
-
-    for result in results:
-        date_scraped = datetime.datetime.strptime(result['date_scraped'], "%Y-%m-%dT%H:%M:%S.%f")
-        formatted_date_s = date_scraped.strftime("%Y-%m-%d %H:%M")
-        date_posted = datetime.datetime.strptime(result['date_posted'], "%Y-%m-%dT%H:%M:%S.%f")
-        formatted_date_p = date_posted.strftime("%Y-%m-%d %H:%M")
-
-
-        image_response = requests.get(result['image_link'])
-        image = Image.open(io.BytesIO(image_response.content))
-        thumbnail_size = (400, 400) 
-        image = image.resize(thumbnail_size)
-        thumbnail_bytes = io.BytesIO()
-        image.save(thumbnail_bytes, format='PNG')
-        thumbnail_bytes.seek(0)
-
-        embed = Embed(title=f"{result['name']}", description=f"Article ID: {result['id']}\nScraping Date: {formatted_date_s}\nDate Posted: {formatted_date_p}", color=discord.Color.blue())
-        embed.add_field(name="Details Link", value=f"{result['details_link']}")
-        embed.set_thumbnail(url="attachment://thumbnail.png")  
+        response = requests.get(
+            url="http://localhost:8000/telegrafi/data",
+            params=query_params
+        )
         
-        file = discord.File(thumbnail_bytes, filename='thumbnail.png')
-        
-        if is_private:
-            await username.send(file=file, embed=embed)
-        else:
-            await channel.send(file=file, embed=embed)
+        results = response.json()["results"]
+
+        for result in results:
+            date_scraped = datetime.datetime.strptime(result['date_scraped'], "%Y-%m-%dT%H:%M:%S.%f")
+            formatted_date_s = date_scraped.strftime("%Y-%m-%d %H:%M")
+            date_posted = datetime.datetime.strptime(result['date_posted'], "%Y-%m-%dT%H:%M:%S.%f")
+            formatted_date_p = date_posted.strftime("%Y-%m-%d %H:%M")
+
+
+            image_response = requests.get(result['image_link'])
+            image = Image.open(io.BytesIO(image_response.content))
+            thumbnail_size = (400, 400) 
+            image = image.resize(thumbnail_size)
+            thumbnail_bytes = io.BytesIO()
+            image.save(thumbnail_bytes, format='PNG')
+            thumbnail_bytes.seek(0)
+
+            embed = Embed(title=f"{result['name']}", description=f"Article ID: {result['id']}\nScraping Date: {formatted_date_s}\nDate Posted: {formatted_date_p}", color=discord.Color.blue())
+            embed.add_field(name="Details Link", value=f"{result['details_link']}")
+            embed.set_thumbnail(url="attachment://thumbnail.png")  
+            
+            file = discord.File(thumbnail_bytes, filename='thumbnail.png')
+            
+            if is_private:
+                await username.send(file=file, embed=embed)
+            else:
+                await telegrafi_channel.send(file=file, embed=embed)
+    else:
+        await channel.send("If you are not an admin, this function can be used only in telegrafi channel or in a private chat")
--- app/utils/google_search.py
+++ app/utils/google_search.py
@@ -1,17 +1,26 @@
 import discord
-import asyncio
 from discord import Embed
-from googlesearch import search
+from googleapiclient.discovery import build
 
+API_KEY = "AIzaSyBRDuu2P07VOQBVUtGd4m5KP8JB2wx3_S4"
+CSE_ID = "b5a161b512cf34d20"
 
 async def search_google(p_message_list, channel, username, is_private):
-    query = " ".join(p_message_list[1:])
-    results = list(search(query, num_results=10))[:3]
-    for i, result in enumerate(results):
-        embed = Embed(title="Google search results", color=discord.Color.blue())
-        embed.add_field(name=f"Result {i+1} for '{query}':", value=result)
+    google_channel = discord.utils.get(channel.guild.channels, name="google")
+    if isinstance(channel, discord.DMChannel) or channel.name.lower() == "google" or channel.permissions_for(username).administrator:
+        query = " ".join(p_message_list[1:])
+        service = build("customsearch", "v1", developerKey=API_KEY)
+        result = service.cse().list(q=query, num=6, cx=CSE_ID).execute()
+        items = result.get("items", [])
+        description = ""
+        for i, item in enumerate(items):
+            description += f"Result {i+1} for '{query}': ({item['link']})\n"
+        
+        embed = Embed(title="Google search results", description=description, color=discord.Color.blue())
         if is_private:
             await username.send(embed=embed)
         else:
-            await channel.send(embed=embed)
-        await asyncio.sleep(5)
+            await google_channel.send(embed=embed)
+    else:
+        await channel.send("If you are not an admin, this function can be used only in google channel or in a private chat")
+    
