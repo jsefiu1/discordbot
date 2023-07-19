ba9d983f7d4877c79cebb671355f206bf324bf35
Alzelida Kryeziu <alzelide.k@gmail.com>
2023-07-19 10:04:10 +0200

    WIP-Image link and category added

--- app/api_functions/douglas.py
+++ app/api_functions/douglas.py
@@ -72,23 +72,32 @@ async def data_douglas(p_message_list, channel, username, is_private):
         results = response.json()["results"]
         
         for result in results:
-            name = result["name"]
             price = result["price"]
-            #details_link = result["details_link"]
+            category = result["category"]
             date_scraped = datetime.datetime.strptime(result['date_scraped'], "%Y-%m-%dT%H:%M:%S.%f")
             formatted_date_s = date_scraped.strftime("%Y-%m-%d %H:%M")            
             
             
             embed = Embed(title=f"{result['name']}", description=f"Product ID: {result['id']}\nScraping Date: {formatted_date_s}", color=discord.Color.blue())
-            embed.add_field(name="Name", value=name, inline=False)
-            embed.add_field(name="Date Scraped", value=date_scraped, inline=False)
+            embed.add_field(name="Category", value=category, inline=False)
             embed.add_field(name="Price", value=price, inline=False)
             embed.add_field(name="Details Link", value=f"{result['details_link']}")
-                        
+            embed.set_thumbnail(url="attachment://thumbnail.png")
+            
+            image_response = requests.get(result['image_link'])
+            image = Image.open(io.BytesIO(image_response.content))
+            thumbnail_size = (400, 400) 
+            image = image.resize(thumbnail_size)
+            thumbnail_bytes = io.BytesIO()
+            image.save(thumbnail_bytes, format='PNG')
+            thumbnail_bytes.seek(0)
+              
+            
+            file = discord.File(thumbnail_bytes, filename='thumbnail.png')
             
             if is_private:
-                await username.send(embed=embed)
+                await username.send(file=file, embed=embed)
             else:
-                await douglas_channel.send(embed=embed)
+                await douglas_channel.send(file=file, embed=embed)
         else:
             await channel.send("If you are not an admin, this function can be used only in douglas channel or in a private chat")
