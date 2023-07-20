dcbd9d3e9fbcbae60cfb9c0512ca82a9228be4b9
Jon <jon.fejzullahu.jon@gmail.com>
2023-07-20 09:41:25 +0200

    bug fix

--- app/api_functions/express.py
+++ app/api_functions/express.py
@@ -51,37 +51,49 @@ async def scrape_gazetaexpress(p_message_list, channel, username, is_private):
 
 
 
-async def data_express(p_message_list, channel):
-    query_params={
-        # "title_contains": "",
-        # "offset": 0,
-        
-    }
-    for argument in p_message_list[2:]:
-        
-        splitted_argument=argument.split(":")
-        argument_name=splitted_argument[0]
-        argument_value=splitted_argument[1]
-        query_params[argument_name]=argument_value
+async def data_express(p_message_list, channel, username, is_private):
+
+    if isinstance(channel, discord.DMChannel):
+        express_channel = channel
+    else:
+        express_channel = discord.utils.get(channel.guild.channels, name="express")
+
+    if isinstance(channel, discord.DMChannel) or channel.name.lower() == express_channel.name or channel.permissions_for(username).administrator:
 
 
-    response = requests.get(
-        url="http://localhost:8000/gazetaexpress/data",
-        params=query_params
-    )
+        query_params={}
+        for argument in p_message_list[2:]:
+            
+            splitted_argument=argument.split(":")
+            argument_name=splitted_argument[0]
+            argument_value=splitted_argument[1]
+            query_params[argument_name]=argument_value
 
-    print(response)
-    
-    
-    results = response.json()["results"]
 
-    for result in results:
-        embed = discord.Embed(
-            title=result['name'],
-            description=f"Scraping Date: {result['date_scraped']}",
-            color=discord.Color.blue()
+        response = requests.get(
+            url="http://localhost:8000/gazetaexpress/data",
+            params=query_params
         )
-        details_link = f"[Read More]({result['details']})"
-        embed.add_field(name="", value=details_link, inline=False)
-        embed.set_image(url=result['image'])
-        await channel.send(embed=embed)
+
+        print(response)
+        
+        
+        results = response.json()["results"]
+
+
+        for result in results:
+            embed = discord.Embed(
+                title=result['name'],
+                description=f"Scraping Date: {result['date_scraped']}",
+                color=discord.Color.blue()
+            )
+            details_link = f"[Read More]({result['details']})"
+            embed.add_field(name="", value=details_link, inline=False)
+            embed.set_image(url=result['image'])
+            if is_private:
+                await username.send(embed=embed)
+            else:
+                await express_channel.send(embed=embed)
+            
+    else:
+        await channel.send("If you are not an admin, this function can be used only in express channel or in a private chat")
